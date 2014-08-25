1ccca6126eefd56d8bdad2fb92f29702b45fd175
kishorepolani <Kpolani@172-29-17-112.dynapool.nyu.edu>
2014-08-24 20:55:42 -0400

    updated settings, created app, added south, enabled django admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
+    'south',
+    'django.contrib.admin',
 )
 
 MIDDLEWARE_CLASSES = (
